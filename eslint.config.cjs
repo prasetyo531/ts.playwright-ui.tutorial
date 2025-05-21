bbb92b37af9e1611a1e37b5dea9477d993d84e5f
prasetyo <prasetyo531@gmail.com>
2025-05-21 09:10:32 +0700

    add: new eslint config with max warning

---
+++ eslint.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  files: ['**/*.ts', '**/*.tsx'],
+  languageOptions: {
+    parser: require('@typescript-eslint/parser'),
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+  },
+  plugins: {
+    '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    playwright: require('eslint-plugin-playwright'),
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'playwright/no-conditional-in-test': 'error',
+    'playwright/no-focused-test': 'error',
+    'playwright/no-skipped-test': 'warn',
+    'playwright/no-nested-step': 'warn',
+  }
+};
